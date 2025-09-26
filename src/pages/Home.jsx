987fdd642bc570816a8a3940d25986fb0b97318b
cosuoji <qelvinz@gmail.com>
2025-09-26 23:07:08 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,7 +44,7 @@ export default function Home() {
   return (
     <div className="p-6 max-w-xl mx-auto">
       <h1 className="text-3xl font-bold mb-4 text-center">
-        Search Our Glossary of Slangs
+        Search Our Glossary of Lamba
       </h1>
 
       {/* 🔍 Search Bar */}
